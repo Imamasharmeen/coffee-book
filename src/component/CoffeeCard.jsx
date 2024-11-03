0b9c9ebcab4ba193c8ae98c755b031359f18c8ea
Imamasharmeen <imamasharmeen@gmail.com>
2024-11-03 08:59:06 +0200

    Added useNavigate function in the view all button

--- src/component/CoffeeCard.jsx
+++ src/component/CoffeeCard.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import {  useLoaderData, useParams } from 'react-router-dom'
+import {  useLoaderData, useNavigate, useParams } from 'react-router-dom'
 import Items from './Items'
 
 
 export default function CoffeeCard() {
+    const navigate=useNavigate()
     const[coffees, setCoffees]= useState([])
     const {category}= useParams()
     const data = useLoaderData()
@@ -25,9 +26,13 @@ export default function CoffeeCard() {
   return (
     <div>
        
-       {
+      <div>
+      {
         coffees.map(cards =><Items key={cards.id} cards={cards}></Items>)
        }
+      </div>
+
+       <button onClick={()=> navigate('/coffees')}>View All</button>
     </div>
   )
 }
